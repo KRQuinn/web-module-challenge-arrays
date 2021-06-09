7f3e288ced341a196c07ff53c6534ec490d5c51b
KRQuinn <kr.quinn82@gmail.com>
2021-06-09 10:47:05 -0700

    solved task 7

--- index.js
+++ index.js
@@ -160,8 +160,18 @@ Use the filterByWord function below to do the following:
     DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-    /*your code here*/
+function filterByWord(array, string){
+    //make an empty array so that we can push our flavors to it
+    const filteredFlavors = [];
+    //loop through the entire array
+    for(let i = 0; i < array.length; i++){
+        //if the index contains the string
+        if(array[i].includes(string)){
+            //push that index to the new array
+            filteredFlavors.push(array[i]);
+        }
+    }
+    return filteredFlavors;
 }
 
 
